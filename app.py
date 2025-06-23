27474c53e23fccd5bda2912b1ef2e36e26503c90
Cordell Thompson <cordellt2@gmail.com>
2025-06-23 14:28:04 -0600

    CORS Issue
    Trying to access the api with my frontend causes a CORS issue. This should fix it until I deploy the frontend on github.

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, send_file, jsonify
+from flask_cors import CORS
 import subprocess
 import os
 
 app = Flask(__name__)
+CORS(app)
 
 # Absolute path to raytrace.exe inside build/Release/
 RAYTRACE_EXE = os.path.join(os.getcwd(), 'build', 'raytrace')
